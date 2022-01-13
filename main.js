46bfa2033f441ffdfe7a311d2f900ff2d8231e82
Gustavo Dantas <78984177+Gust4voD4nt4s@users.noreply.github.com>
2022-01-12 23:20:50 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ MENUMOBILE.addEventListener('click', menuResponsivo);
 cancelmenu.addEventListener('click', menuResponsivo);
 
 $('.fullBackground').fullClip({
-  images: ['drink1.jpg', 'picanha-na-brasa.jpg', 'drink3.jpg'],
+  images: ['Drinklaranja.png', 'Picanha.png', 'Drinklaranja2.png'],
   transitionTime: 2000,
   wait: 5000
 });
 
 $('.slider-principal').slick({
   dots: false,
-  infinite: false,
+  infinite: true,
   speed: 300,
   adaptiveHeight: false,
   autoplay: true,
